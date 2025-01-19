c02bca885525b0415fd213bec3aea152908db258
AzizBenIsmail <aziz.270700@gmail.com>
2025-01-19 12:28:19 +0100

    DeleteUser / AddUser / AddUserWithImg

--- src/components/Cards/CardTable.js
+++ src/components/Cards/CardTable.js
@@ -1,13 +1,19 @@
-import React, { useState, useCallback , useEffect } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import PropTypes from "prop-types";
 
 // components
 
-import TableDropdown from "components/Dropdowns/TableDropdown.js";
-
-import { getAllUsers } from "../../services/apiUser";
+import { getAllUsers, deleteUser , addUser , addUserwithImg} from "../../services/apiUser";
 export default function CardTable({ color }) {
   const [users, setUsers] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [newUser , setNewUser] = useState({
+    firstName : "",
+    lastName:"",
+    email : "",
+    password : "",
+  })
+const [image,setImage] = useState()
 
   const getUsers = useCallback(async () => {
     await getAllUsers()
@@ -17,7 +23,63 @@ export default function CardTable({ color }) {
       .catch((err) => console.log(err));
   }, []);
 
-  useEffect(() => { getUsers()}, [getUsers])
+  useEffect(() => {
+    getUsers();
+  }, [getUsers]);
+
+  const handleDeleteUser = async (id) => {
+    try {
+      const confimDelete = window.confirm("Are you sure you want to delete");
+      if (!confimDelete) {
+        return;
+      }
+      await deleteUser(id);
+      getUsers();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  const handleChange = (e) => {
+    const {name , value} = e.target;
+    setNewUser({...newUser , [name]: value});
+    console.log(newUser);
+  }
+
+  const handleAddUser = async () => {
+    try {
+      setShowModal(false);
+      await addUser(newUser)
+      getUsers();
+
+    } catch (error) {
+      console.log(error); 
+    }
+  }
+
+  const handleChangeFile = (e) => {
+    setImage(e.target.files[0]);
+  }
+
+  const formData = new FormData();
+
+  const handleAddUserWithIMG = async () => {
+    try {
+      formData.append('email',newUser.email)
+      formData.append('password',newUser.password)
+      formData.append('firstName', newUser.firstName)
+      formData.append('lastName',newUser.lastName)
+      formData.append('image_user',image)
+      setShowModal(false);
+      await addUserwithImg(formData)
+      getUsers();
+
+    } catch (error) {
+      console.log(error); 
+    }
+  }
+
+
   return (
     <>
       <div
@@ -25,8 +87,13 @@ export default function CardTable({ color }) {
           "relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded " +
           (color === "light" ? "bg-white" : "bg-lightBlue-900 text-white")
         }
-      >               
+      >
         <div className="rounded-t mb-0 px-4 py-3 border-0">
+        <button className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button"
+            onClick={()=> setShowModal(true)}
+          >
+            Add User
+          </button>
           <div className="flex flex-wrap items-center">
             <div className="relative w-full px-4 max-w-full flex-grow flex-1">
               <h3
@@ -116,52 +183,178 @@ export default function CardTable({ color }) {
               </tr>
             </thead>
             <tbody>
-              {users.map((user,index) => (
-              <tr key={index}>
-                <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
-                  <img
-                    src={`http://localhost:5000/images/Users/${user.user_image}`}
-                    className="h-12 w-12 bg-white rounded-full border"
-                    alt="..."
-                  ></img>{" "}
-                  <span
-                    className={
-                      "ml-3 font-bold " +
-                      +(color === "light" ? "text-blueGray-600" : "text-white")
-                    }
-                  >
-                    {user.firstName}
-                  </span>
-                </th>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                {user.lastName}
-
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                {user.email}
-
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                {user.age}
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                {user.createdAt}
-                
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
-                  <button
-                    className="bg-red-500 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                  >
-                    Supprimer
-                  </button>
-                </td>
-              </tr>
+              {users.map((user, index) => (
+                <tr key={index}>
+                  <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
+                    <img
+                      src={`http://localhost:5000/images/Users/${user.user_image}`}
+                      className="h-12 w-12 bg-white rounded-full border"
+                      alt="..."
+                    ></img>{" "}
+                    <span
+                      className={
+                        "ml-3 font-bold " +
+                        +(color === "light"
+                          ? "text-blueGray-600"
+                          : "text-white")
+                      }
+                    >
+                      {user.firstName}
+                    </span>
+                  </th>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    {user.lastName}
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    {user.email}
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    {user.age}
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    {user.createdAt}
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
+                    <button
+                      className="bg-red-500 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      type="button"
+                      onClick={() => {
+                        handleDeleteUser(user._id);
+                      }}
+                    >
+                      Supprimer
+                    </button>
+                  </td>
+                </tr>
               ))}
             </tbody>
           </table>
         </div>
+        {showModal && (
+        <div className=" inset-0 flex items-center justify-center ml-7 z-50 bg-black bg-opacity-50">
+          <div className="bg-white p-6  rounded shadow-lg w-96">
+            <form>            
+              <h6 className="text-blueGray-400 text-sm mt-3 ml-2 mb-6 font-bold uppercase">
+              Ajouter un utilisateur
+            </h6>
+            <div className="flex flex-wrap">              
+              <div className="w-full lg:w-6/12 px-4">
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    htmlFor="grid-password"
+                  >
+                    Email address
+                  </label>
+                  <input
+                    type="email"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    placeholder="Your email address"
+                    name="email"
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div className="w-full lg:w-6/12 px-4">
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    htmlFor="grid-password"
+                  >
+                    Password 
+                  </label>
+                  <input
+                    type="password"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    placeholder="your password"
+                    name="password"
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div className="w-full lg:w-6/12 px-4">
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    htmlFor="grid-password"
+                  >
+                    First Name
+                  </label>
+                  <input
+                    type="text"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    placeholder="First Name"
+                    name="firstName"
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div className="w-full lg:w-6/12 px-4">
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    htmlFor="grid-password"
+                  >
+                    Last Name
+                  </label>
+                  <input
+                    type="text"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    placeholder="Last Name"
+                    name="lastName"
+                    onChange={handleChange}
+
+                  />
+                </div>
+              </div>
+              <div className="w-full lg:w-6/12 px-4">
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    htmlFor="grid-password"
+                  >
+                    File
+                  </label>
+                  <input
+                    type="file"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    name="image_user"
+                    onChange={handleChangeFile}
+
+                  />
+                </div>
+              </div>
+            </div>  
+            <div className="flex justify-end mr-2 mb-2">
+                <button
+                  type="button"
+                  className="bg-gray-300 text-black px-4 py-2 rounded mr-2"
+                  onClick={() => setShowModal(false)}
+                >
+                  Annuler
+                </button>
+                <button
+                  type="button"
+                  className="bg-lightBlue-500 text-white px-4 py-2 rounded"
+                onClick={handleAddUser}
+                >
+                  Ajouter
+                </button>
+                <button
+                  type="button"
+                  className="bg-lightBlue-500 text-white px-4 py-2 rounded"
+                onClick={handleAddUserWithIMG}
+                >
+                  Ajouter avec image
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
       </div>
+
+
     </>
   );
 }
--- src/services/apiUser.js
+++ src/services/apiUser.js
@@ -5,3 +5,17 @@ const apiURL = 'http://localhost:5000/users';
 export async function getAllUsers (){
     return await axios.get(`${apiURL}/getAllUsers`)
 }
+
+export async function deleteUser(id){
+    return await axios.delete(`${apiURL}/deleteUser/${id}`)
+}
+
+export async function addUser(userData){
+    return await axios.post(`${apiURL}/addUser`, userData)
+}
+
+export async function addUserwithImg(formData){
+    return await axios.post(`${apiURL}/addClient`, formData,{
+        headers: {'Content-Type': 'multipart/form-data'},
+    })
+}
